ea5a602db7c93c1080173bda84775475751ec85d
bilal-sajid <ahmadbilalsajid01@gmail.com>
2024-10-07 11:04:28 -0400

    Basic Navbar for Small and Large Devices

--- mern-client/src/components/Navbar.jsx
+++ mern-client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom';
 
 // React Icons
-import {FaBlog} from "react-icons/fa6";
+import {FaBarsStaggered, FaBlog, FaXmark} from "react-icons/fa6";
 
 const Navbar = () => {
     const [isMenuOpen,setIsMenuOpen] = useState(false);
@@ -41,9 +41,9 @@ const Navbar = () => {
         {link:"Blog", path:"/blog"},
     ]
   return (
-    <header>
-        <nav>
-            <div>
+    <header className='w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300'>
+        <nav className={`py-4 lg:px-24 px-4 ${isSticky ? "sticky top-0 left-0 right-0 bg-blue-300" : ""}`}>
+            <div className='flex justify-between items-center text-base gap-8'>
                 {/* {Logo} */}
                 <Link to="/" className='text-2xl font-bold text-blue-700 flex items-center gap-2'><FaBlog className='inline-block'/>Books</Link>
 
@@ -56,6 +56,32 @@ const Navbar = () => {
                         </Link>))
                     }
                 </ul>
+
+                {/* {Button For Large Devices} */}
+                <div className='space-x-12 hidden lg:flex items-center'>
+                    <button>
+                        <FaBarsStaggered className='w-5 hover:text-blue-700'/>
+                    </button>
+                </div>
+
+                {/* Menu Button for Mobile Devices */}
+                <div className='md:hidden'>
+                    <button onClick={toggleMenu} className='text-black focus:outline-none'>
+                        {
+                            isMenuOpen ? <FaXmark className='h-5 w-5 text-black'/> : <FaBarsStaggered/>
+                        }
+                    </button>
+                </div>
+            </div>
+
+            {/* Navigation Items for Small Devices */}
+            <div className={`space-y-4 px-4 mt-16 py-7 bg-blue-700 ${isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"}`}>
+                {
+                navItems.map(({link, path}) => (
+                    <Link key = {path} to={path} className='block text-base text-white uppercase cursor-pointer'>{
+                        link}
+                    </Link>))
+                }
             </div>
         </nav>
     </header>
--- mern-client/src/home/home.jsx
+++ mern-client/src/home/home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className=''>
+      <div className='h-screen'> Home
+        </div>
+        <div className='h-screen bg-red-700'></div>
+      Home</div>
   )
 }
 

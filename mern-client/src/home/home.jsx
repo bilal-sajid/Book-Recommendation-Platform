ae42cca9654eaa957f2bbd4b7080748eca6de53c
bilal-sajid <ahmadbilalsajid01@gmail.com>
2024-10-07 11:18:10 -0400

    Banner For Homepage
    Created the left side of the Banner that will be visible at the Home Page

--- mern-client/src/home/home.jsx
+++ mern-client/src/home/home.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import Banner from '../components/Banner'
 
 function Home() {
   return (
-    <div className=''>
-      <div className='h-screen'> Home
-        </div>
-        <div className='h-screen bg-red-700'></div>
-      Home</div>
+    <div>
+      <Banner/>
+    </div>
   )
 }
 

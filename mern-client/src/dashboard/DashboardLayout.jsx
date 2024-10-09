ba221a1664c0b701d5f2d01eac44a2c9ee638cc7
bilal-sajid <ahmadbilalsajid01@gmail.com>
2024-10-09 13:24:05 -0400

    Update DashboardLayout.jsx

--- mern-client/src/dashboard/DashboardLayout.jsx
+++ mern-client/src/dashboard/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import SideBarComponent from './Sidebar'
+import SideBarComponent from './SideBarComponent'
 
 const DashboardLayout = () => {
   return (

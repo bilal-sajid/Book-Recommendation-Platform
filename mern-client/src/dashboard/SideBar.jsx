c9f157d5ae2a79f254a42c0283c7fdd3b5c0b080
bilal-sajid <ahmadbilalsajid01@gmail.com>
2024-10-09 13:15:45 -0400

    Update SideBar.jsx

--- mern-client/src/dashboard/SideBar.jsx
+++ mern-client/src/dashboard/SideBar.jsx
@@ -1,20 +1,17 @@
 import React, { useContext } from 'react'
 import { Sidebar } from "flowbite-react";
-import { BiBuoy } from "react-icons/bi";
-import { HiArrowSmRight, HiChartPie, HiInbox, HiOutlineCloudUpload, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+import { HiArrowSmRight, HiChartPie, HiInbox, HiOutlineCloudUpload, HiTable } from "react-icons/hi";
 import {AuthContext} from "../contexts/AuthProvider"
-
-// import userImage from "../assets/ProfileImage.jpeg"
 
 const SideBar = () => {
 
   const {user} = useContext(AuthContext)
-  // console.log(user)
 
   return (
     <Sidebar aria-label="Sidebar with content separator example">
     
     <Sidebar.Logo href="/" img={user?.photoURL}> 
+
       {/* imgAlt="Flowbite logo"> */}
         {
           user?.displayName || "Demo User"
@@ -32,12 +29,6 @@ const SideBar = () => {
           <Sidebar.Item href="/admin/dashboard/manage" icon={HiInbox}>
             Manage Books
           </Sidebar.Item>
-          {/* <Sidebar.Item href="#" icon={HiUser}>
-            Users
-          </Sidebar.Item> */}
-          {/* <Sidebar.Item href="#" icon={HiShoppingBag}>
-            Products
-          </Sidebar.Item> */}
           <Sidebar.Item href="/login" icon={HiArrowSmRight}>
             Sign In
           </Sidebar.Item>

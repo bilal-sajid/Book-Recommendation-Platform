b3a4121910a1f07238a6c4e3b47c1f4ea302e00f
bilal-sajid <ahmadbilalsajid01@gmail.com>
2024-10-10 17:21:28 -0400

    Update SideBarComponent.jsx

--- mern-client/src/dashboard/SideBarComponent.jsx
+++ mern-client/src/dashboard/SideBarComponent.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { Sidebar } from "flowbite-react";
 import { HiArrowSmRight, HiChartPie, HiInbox, HiOutlineCloudUpload, HiTable } from "react-icons/hi";
 import {AuthContext} from "../contexts/AuthProvider"
+import { Link } from 'react-router-dom';
 
 
 
@@ -22,12 +23,29 @@ const SideBarComponent = () => {
 
       <Sidebar.Items>
         <Sidebar.ItemGroup>
+
+
+          <Sidebar.Item>
+            <Link to="/admin/dashboard" className="flex items-center text-gray-800 hover:text-blue-600 transition-all duration-300">
+              <HiChartPie className="mr-3" />
+              Dashboard
+            </Link>
+          </Sidebar.Item>
+
+          <Sidebar.Item>
+            <Link to="/admin/dashboard/upload" className="flex items-center text-gray-800 hover:text-blue-600 transition-all duration-300">
+              <HiOutlineCloudUpload className="mr-3" />
+              Upload Book
+            </Link>
+          </Sidebar.Item>
+
+{/*           
           <Sidebar.Item href="/admin/dashboard" icon={HiChartPie}>
             Dashboard
-          </Sidebar.Item>
-          <Sidebar.Item href="/admin/dashboard/upload" icon={HiOutlineCloudUpload}>
+          </Sidebar.Item> */}
+          {/* <Sidebar.Item href="/admin/dashboard/upload" icon={HiOutlineCloudUpload}>
             Upload Book
-          </Sidebar.Item>
+          </Sidebar.Item> */}
           <Sidebar.Item href="/admin/dashboard/manage" icon={HiInbox}>
             Manage Books
           </Sidebar.Item>

dc3b87538fd4df36ccaee81367abd0249ad0d256
bilal-sajid <ahmadbilalsajid01@gmail.com>
2024-10-11 21:15:45 -0400

    Update Banner.jsx

--- mern-client/src/components/Banner.jsx
+++ mern-client/src/components/Banner.jsx
@@ -49,7 +49,7 @@ const Banner = () => {
 
       <div className="mt-10 justify-center">
         <h1 className="w-1/2 text-center bg-blue-100 text-blue-800 p-4 rounded-lg shadow-lg">
-          If the page hasn't fully loaded, please wait a few minutes. We're gathering all the best book recommendations for you, and they'll appear shortly! Thank you for your patience. 😊
+          If the page hasn't fully loaded, please wait a few minutes. We're gathering all the best book recommendations for you, and they'll appear within a minute! Thank you for your patience. 😊
         </h1>
       </div>
 
